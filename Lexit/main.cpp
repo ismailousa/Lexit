7502224f44aec6bf1eb147f2d312e11f85129898
DESKTOP-LL4FE81\isma_ <isma_ilou@hotmail.com>
2018-01-10 01:47:02 +0300

    Texture mapping, labyrinth entry and exit added

--- Lexit/main.cpp
+++ Lexit/main.cpp
@@ -1,12 +1,31 @@
+﻿//1131603702
+//Isma-Ilou SADOU
+
+//kamera yon tuslari ile kontrol edilir
+//Oyuncu W-S-A-D tuslari ile hareket ettirilir
+//Isik L tusu ile acilip kapanir
+//Zoom 1 ve 2 tuslariile yapilir
+
+
 #include "GL\freeglut.h"
 #include "GL\glut.h"
 #include <iostream>
+#include "main.h"
 
 double rotate_y = 0;
 double rotate_x = 0;
-double player_x = -0.55;
+double player_x = -0.65;
 double player_z = -0.55;
 double zoom = 1.0;
+bool won = false;
+
+GLuint texName;				
+GLubyte *data;					
+								
+GLubyte header[54];				
+GLuint dataPos;					
+GLuint width, height;			
+GLuint imageSize;
 
 bool light = false;
 
@@ -34,6 +53,23 @@ void specialKeys(int key, int x, int y) {
 
 void renderScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glDisable(GL_DEPTH_TEST);
+	glPushMatrix();
+	glRasterPos2f(0.5, 0.7);
+	char* string;
+	if (won)
+		string = "Kazandiniz !!";
+	else
+		string = "Cikisa gidin";
+	int len = (int)strlen(string);
+	glPopMatrix();
+	//loop to display character by character
+	for (int i = 0; i < len; i++)
+	{
+		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, string[i]);
+	}
+	glEnable(GL_DEPTH_TEST);
+
 	glEnable(GL_LIGHT0);
 
 	// Create light components
@@ -48,6 +84,9 @@ void renderScene(void) {
 	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
 	glLightfv(GL_LIGHT0, GL_POSITION, position);
 
+	glEnable(GL_TEXTURE_2D);    
+								
+	glBindTexture(GL_TEXTURE_2D, texName);
 	glLoadIdentity();
 
 	// Rotate when user changes rotate_x and rotate_y
@@ -59,10 +98,10 @@ void renderScene(void) {
 	//FRONT
 	glBegin(GL_POLYGON);
 	glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.7, -0.5, -0.7);      // P1 is red
-	glVertex3f(0.7, -0.4, -0.7);      // P2 is green
-	glVertex3f(-0.7, -0.4, -0.7);      // P3 is blue
-	glVertex3f(-0.7, -0.5, -0.7);      // P4 is purple
+	glVertex3f(0.7, -0.5, -0.7);    
+	glVertex3f(0.7, -0.4, -0.7);    
+	glVertex3f(-0.7, -0.4, -0.7);   
+	glVertex3f(-0.7, -0.5, -0.7);   
 
 	glEnd();
 
@@ -102,20 +141,16 @@ void renderScene(void) {
 	glVertex3f(-0.7, -0.4, -0.6);
 	glEnd();
 
-	// Red side - BOTTOM
-	/*glBegin(GL_POLYGON);
-	glColor3ub(172, 163, 163);
-	glVertex3f(0.7, -0.5, -0.7);
-	glVertex3f(0.7, -0.5, 0.7);
-	glVertex3f(-0.7, -0.5, 0.7);
-	glVertex3f(-0.7, -0.5, -075);
-	glEnd();*/
-
+	glBindTexture(GL_TEXTURE_2D, texName);
 	glBegin(GL_POLYGON);
 	glColor3ub(172, 163, 163);
+	glTexCoord2f(0, 0);
 	glVertex3f(0.7, -0.5, -0.7);
+	glTexCoord2f(0, 1);
 	glVertex3f(0.7, -0.5, 0.7);
+	glTexCoord2f(1, 1);
 	glVertex3f(-0.7, -0.5, 0.7);
+	glTexCoord2f(1, 0);
 	glVertex3f(-0.7, -0.5, -0.7);
 	glEnd();
 #pragma endregion
@@ -123,8 +158,8 @@ void renderScene(void) {
 	//FRONT
 	glBegin(GL_POLYGON);
 	glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.7, -0.5, 0.7);    
-	glVertex3f(0.7, -0.4, 0.7);    
+	glVertex3f(0.5, -0.5, 0.7);    
+	glVertex3f(0.5, -0.4, 0.7);    
 	glVertex3f(-0.7, -0.4, 0.7);   
 	glVertex3f(-0.7, -0.5, 0.7);   
 
@@ -133,8 +168,8 @@ void renderScene(void) {
 	//BACK
 	glBegin(GL_POLYGON);
 	glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.7, -0.5, 0.6);
-	glVertex3f(0.7, -0.4, 0.6);
+	glVertex3f(0.5, -0.5, 0.6);
+	glVertex3f(0.5, -0.4, 0.6);
 	glVertex3f(-0.7, -0.4, 0.6);
 	glVertex3f(-0.7, -0.5, 0.6);
 	glEnd();
@@ -142,10 +177,10 @@ void renderScene(void) {
 	// Purple side - RIGHT
 	glBegin(GL_POLYGON);
 	glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.7, -0.5, 0.7);
-	glVertex3f(0.7, -0.4, 0.7);
-	glVertex3f(0.7, -0.4, 0.6);
-	glVertex3f(0.7, -0.5, 0.6);
+	glVertex3f(0.5, -0.5, 0.7);
+	glVertex3f(0.5, -0.4, 0.7);
+	glVertex3f(0.5, -0.4, 0.6);
+	glVertex3f(0.5, -0.5, 0.6);
 	glEnd();
 	//LEFT
 	glBegin(GL_POLYGON);
@@ -159,8 +194,8 @@ void renderScene(void) {
 	//TOP
 	glBegin(GL_POLYGON);
 	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(0.7, -0.4, 0.6);
-	glVertex3f(0.7, -0.4, 0.7);
+	glVertex3f(0.5, -0.4, 0.6);
+	glVertex3f(0.5, -0.4, 0.7);
 	glVertex3f(-0.7, -0.4, 0.7);
 	glVertex3f(-0.7, -0.4, 0.6);
 	glEnd();
@@ -169,10 +204,10 @@ void renderScene(void) {
 	//FRONT
 	glBegin(GL_POLYGON);
 	glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(-0.6, -0.5, -0.7);
-	glVertex3f(-0.6, -0.4, -0.7);
-	glVertex3f(-0.7, -0.4, -0.7);
-	glVertex3f(-0.7, -0.5, -0.7);
+	glVertex3f(-0.6, -0.5, -0.5);
+	glVertex3f(-0.6, -0.4, -0.5);
+	glVertex3f(-0.7, -0.4, -0.5);
+	glVertex3f(-0.7, -0.5, -0.5);
 
 	glEnd();
 
@@ -190,14 +225,14 @@ void renderScene(void) {
 	glColor3f(1.0, 0.0, 1.0);
 	glVertex3f(-0.6, -0.5, 0.7);
 	glVertex3f(-0.6, -0.4, 0.7);
-	glVertex3f(-0.6, -0.4, -0.7);
-	glVertex3f(-0.6, -0.5, -0.7);
+	glVertex3f(-0.6, -0.4, -0.5);
+	glVertex3f(-0.6, -0.5, -0.5);
 	glEnd();
 	//LEFT
 	glBegin(GL_POLYGON);
 	glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(-0.7, -0.5, -0.7);
-	glVertex3f(-0.7, -0.4, -0.7);
+	glVertex3f(-0.7, -0.5, -0.5);
+	glVertex3f(-0.7, -0.4, -0.5);
 	glVertex3f(-0.7, -0.4, 0.7);
 	glVertex3f(-0.7, -0.5, 0.7);
 	glEnd();
@@ -205,10 +240,10 @@ void renderScene(void) {
 	//TOP
 	glBegin(GL_POLYGON);
 	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(-0.6, -0.4, -0.7);
+	glVertex3f(-0.6, -0.4, -0.5);
 	glVertex3f(-0.6, -0.4, 0.7);
 	glVertex3f(-0.7, -0.4, 0.7);
-	glVertex3f(-0.7, -0.4, -0.7);
+	glVertex3f(-0.7, -0.4, -0.5);
 	glEnd();
 #pragma endregion
 #pragma region Wall4
@@ -1136,9 +1171,50 @@ void renderScene(void) {
 	glColor3f(1, 0, 0);
 	glutSolidSphere(0.04, 20, 20);
 	glPopMatrix();
+	
 
 	glFlush();
 	glutSwapBuffers();
+}
+
+GLuint loadBMP_custom(const char * imagepath) {
+	FILE * file;
+	fopen_s(&file, imagepath, "rb");
+	if (!file) {
+		printf("Image could not be opened\n");
+		return 0;
+	}
+	if (fread(header, 1, 54, file) != 54) {
+		printf("Not a correct BMP file\n");
+		return 0;
+	}
+	if (header[0] != 'B' || header[1] != 'M') {
+		printf("Not a correct BMP file\n");
+		return 0;
+	}
+	dataPos = *(int*)&(header[0x0A]);
+	imageSize = *(int*)&(header[0x22]);
+	width = *(int*)&(header[0x12]);
+	height = *(int*)&(header[0x16]);
+
+	if (imageSize == 0)    imageSize = width*height * 3;
+	if (dataPos == 0)      dataPos = 54;
+
+										 
+	data = new unsigned char[imageSize];
+	fread(data, 1, imageSize, file);
+	fclose(file);
+}
+
+
+void DokuKaplama(void) {
+	glGenTextures(1, &texName); 
+
+	loadBMP_custom("mcqueen.bmp"); 
+	glBindTexture(GL_TEXTURE_2D, texName);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, data);
 }
 
 void keyboard(unsigned char key, int x, int y)
@@ -1147,8 +1223,11 @@ void keyboard(unsigned char key, int x, int y)
 		player_x -= 0.1;
 	else if (key == 'd' || key == 'D')
 		player_x += 0.1;
-	else if (key == 'w' || key == 'W')
+	else if (key == 'w' || key == 'W') {
 		player_z += 0.1;
+		if (player_x > 0.5 && player_z > 0.6)
+			won = true;
+	}
 	else if (key == 's' || key == 'S')
 		player_z -= 0.1;
 	else if (key == '1')
@@ -1178,6 +1257,7 @@ int main(int argc, char **argv) {
 	glutDisplayFunc(renderScene);
 	glutSpecialFunc(specialKeys);
 	glutKeyboardFunc(keyboard);
+	DokuKaplama();
 	glutMainLoop();
 	return 0;
 }
